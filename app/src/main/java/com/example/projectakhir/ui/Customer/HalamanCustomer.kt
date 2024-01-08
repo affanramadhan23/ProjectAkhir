8ff7054e7d32bc60cb4cc0fc5a323c5e28957210
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-09 03:02:45 +0700

    membuat function ListCustom  untuk menampilkan daftar Customer dengan LazyColumn dan dapat di klik

--- app/src/main/java/com/example/projectakhir/ui/Customer/HalamanCustomer.kt
+++ app/src/main/java/com/example/projectakhir/ui/Customer/HalamanCustomer.kt
@@ -1,11 +1,14 @@
 package com.example.projectakhir.ui.Customer
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Phone
@@ -55,6 +58,26 @@ fun BodyHomeCustomer(
         }
     }
 }
+@Composable
+fun ListCustomer(
+    itemCustomer: List<Customer>,
+    modifier: Modifier = Modifier,
+    onItemClick: (Customer) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        this.items(itemCustomer, key = { it.id }) {customer ->
+            DataCustomer(
+                customer = customer,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onItemClick(customer) }
+            )
+        }
+    }
+}
+
 @Composable
 fun DataCustomer(
     customer: Customer,
