bcf840ddead32a4ed3a42c97b7e6f9cc3d0dfd39
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-08 21:19:47 +0700

    membuat implementasi AppContainer menggunakan Firebase Firestore untuk menyediakan AnggotaRepository

--- app/src/main/java/com/example/projectakhir/Data/pinjolContainer.kt
+++ app/src/main/java/com/example/projectakhir/Data/pinjolContainer.kt
@@ -1,5 +1,15 @@
 package com.example.projectakhir.Data
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 interface AppContainer {
     val customerRepository: CustomerRepository
+}
+
+class pinjolContainer : AppContainer {
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    override val customerRepository: CustomerRepository by lazy {
+        CustomerRepositoryImpl(firestore)
+    }
 }
