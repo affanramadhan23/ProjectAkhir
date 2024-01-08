a0af84a1face7741244e2a8af318254eac3c193a
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-08 20:40:10 +0700

    membuat class AnggotaRepositoryImpl

--- app/src/main/java/com/example/projectakhir/Data/pinjolrepository.kt
+++ app/src/main/java/com/example/projectakhir/Data/pinjolrepository.kt
@@ -1,6 +1,7 @@
 package com.example.projectakhir.Data
 
 import com.example.projectakhir.model.Customer
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 
 
@@ -10,4 +11,26 @@ interface CustomerRepository {
     suspend fun update(customer: Customer)
     suspend fun delete(customerId: String)
     fun getCustomerById(customerId: String): Flow<Customer>
+}
+
+class CustomerRepositoryImpl(private val firestore: FirebaseFirestore) : CustomerRepository {
+    override fun getAll(): Flow<List<Customer>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun save(customer: Customer): String {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(customer: Customer) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(customerId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCustomerById(anggotaId: String): Flow<Customer> {
+        TODO("Not yet implemented")
+    }
 }
