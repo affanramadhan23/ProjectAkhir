aae3fc48d3b80c72fa911df476a7a1c5fae459e1
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-08 21:32:26 +0700

    membuat object DestinasiHome yang mengimplementasikan DestinasiNavigasi

---
+++ app/src/main/java/com/example/projectakhir/ui/Customer/HalamanCustomer.kt
@@ -0,0 +1,7 @@
+package com.example.projectakhir.ui.Customer
+
+import com.example.projectakhir.Navigation.DestinasiNavigasi
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Customer"
+}
