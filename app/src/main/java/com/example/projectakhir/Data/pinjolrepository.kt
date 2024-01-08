e85fa0e6c4aa37f2d2e1d5a144dff0032382909f
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-08 20:43:57 +0700

    membuat code untuk mengambil semua data Anggota dari Firestore dengan urutan nama

--- app/src/main/java/com/example/projectakhir/Data/pinjolrepository.kt
+++ app/src/main/java/com/example/projectakhir/Data/pinjolrepository.kt
@@ -2,7 +2,12 @@ package com.example.projectakhir.Data
 
 import com.example.projectakhir.model.Customer
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.tasks.await
 
 
 interface CustomerRepository {
@@ -14,9 +19,14 @@ interface CustomerRepository {
 }
 
 class CustomerRepositoryImpl(private val firestore: FirebaseFirestore) : CustomerRepository {
-    override fun getAll(): Flow<List<Customer>> {
-        TODO("Not yet implemented")
-    }
+    override fun getAll(): Flow<List<Customer>> = flow {
+        val snapshot = firestore.collection("Customer")
+            .orderBy("nama", Query.Direction.ASCENDING)
+            .get()
+            .await()
+        val anggota = snapshot.toObjects(Customer::class.java)
+        emit(anggota)
+    }.flowOn(Dispatchers.IO)
 
     override suspend fun save(customer: Customer): String {
         TODO("Not yet implemented")
