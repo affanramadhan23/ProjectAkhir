969be2c319056cc1c71676f0020c0b36497b4220
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-08 21:39:54 +0700

    membuat function AnggotaScreen

--- app/src/main/java/com/example/projectakhir/ui/Customer/HalamanCustomer.kt
+++ app/src/main/java/com/example/projectakhir/ui/Customer/HalamanCustomer.kt
@@ -1,7 +1,20 @@
 package com.example.projectakhir.ui.Customer
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.projectakhir.Data.CustomerRepository
 import com.example.projectakhir.Navigation.DestinasiNavigasi
+
+
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "Customer"
 }
+
+
+@Composable
+fun CustomerScreen(
+    navigateToItemEntryCustomerRepository: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+) {}
