a9d274406e1f22d07e778756bc145d1d482f8c68
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-10 01:37:42 +0700

    membuat function FormInputAnggota dan parameter nya

--- app/src/main/java/com/example/projectakhir/ui/AddCustomer/AddCustomer.kt
+++ app/src/main/java/com/example/projectakhir/ui/AddCustomer/AddCustomer.kt
@@ -1,10 +1,20 @@
 package com.example.projectakhir.ui.AddCustomer
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.projectakhir.Navigation.DestinasiNavigasi
+import com.example.projectakhir.ui.AddEventCustomer
 
 object AddCustomer {
     object DestinasiEntry : DestinasiNavigasi {
         override val route = "item_entry"
         override val titleRes = "Entry Customer"
     }
+    @Composable
+    fun FormInputCustomer(
+        addEventCustomer: AddEventCustomer,
+        modifier: Modifier = Modifier,
+        onValueChange: (AddEventCustomer) -> Unit = {},
+        enabled: Boolean = true
+    ) {}
 }
