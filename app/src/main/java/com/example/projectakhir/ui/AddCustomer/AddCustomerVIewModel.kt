a2306e1641ce9459cf4d997838b6548782b67aa3
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-10 01:30:58 +0700

    menambahkan function suspending untuk menambahkan Customer menggunakan data dari state UI AddCustomerViewModel

--- app/src/main/java/com/example/projectakhir/ui/AddCustomer/AddCustomerVIewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/AddCustomer/AddCustomerVIewModel.kt
@@ -7,11 +7,16 @@ import androidx.lifecycle.ViewModel
 import com.example.projectakhir.Data.CustomerRepository
 import com.example.projectakhir.ui.AddEventCustomer
 import com.example.projectakhir.ui.AddUIStateCustomer
+import com.example.projectakhir.ui.toCustomer
 
 class AddCustomerViewModel(private val customerRepository: CustomerRepository) : ViewModel() {
     var addUIStateCustomer by mutableStateOf(AddUIStateCustomer())
         private set
     fun updateAddUIStateCustomer(addEventCustomer: AddEventCustomer) {
         addUIStateCustomer = AddUIStateCustomer(addEventCustomer = addEventCustomer)
+    }
+
+    suspend fun addCustomer() {
+        customerRepository.save(addUIStateCustomer.addEventCustomer.toCustomer())
     }
 }
