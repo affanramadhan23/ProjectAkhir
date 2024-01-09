14549ad63a7f1311c4b9d17a2e63f78a2fc25262
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-09 15:02:25 +0700

    membuat StateFlow homeUIState dari repository Customer dengan pemetaan ke HomeUIState dan timeout 6,000L menggunakan stateIn

--- app/src/main/java/com/example/projectakhir/ui/Customer/HalamanCustomerViewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/Customer/HalamanCustomerViewModel.kt
@@ -1,9 +1,16 @@
 package com.example.projectakhir.ui.Customer
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.projectakhir.Data.CustomerRepository
 import com.example.projectakhir.model.Customer
+import com.example.projectakhir.ui.HomeUIState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 sealed class CustomerUIState {
     data class Success(val customer: Flow<List<Customer>>) : CustomerUIState()
@@ -14,4 +21,15 @@ class HomeViewModel(private val CustomerRepository: CustomerRepository) : ViewMo
     companion object {
         private const val TIMEOUT_MILLIS = 6_000L
     }
+
+    val homeUIState: StateFlow<HomeUIState> = CustomerRepository.getAll()
+        .filterNotNull()
+        .map {
+            HomeUIState (listAnggota = it.toList(), it.size)
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUIState()
+        )
 }
