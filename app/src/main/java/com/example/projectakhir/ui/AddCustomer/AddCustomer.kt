5a90b7806cf15dc02ddc24895679f47c4d05e0b0
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-10 06:37:47 +0700

    menambah parameter dan Scaffold dengan EntryBodyCustomer untuk form input Customer dan tombol submit menggunakan AddCustomerViewModel

--- app/src/main/java/com/example/projectakhir/ui/AddCustomer/AddCustomer.kt
+++ app/src/main/java/com/example/projectakhir/ui/AddCustomer/AddCustomer.kt
@@ -4,21 +4,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.projectakhir.Navigation.DestinasiNavigasi
 import com.example.projectakhir.ui.AddEventCustomer
 import com.example.projectakhir.ui.AddUIStateCustomer
+import com.example.projectakhir.ui.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object AddCustomer {
     object DestinasiEntry : DestinasiNavigasi {
@@ -30,9 +37,29 @@ object AddCustomer {
     fun AddCustomer(
         navigateBack: () -> Unit,
         modifier: Modifier = Modifier,
+        addCustomerViewModel: AddCustomerViewModel = viewModel(factory = PenyediaViewModel.Factory)
     ) {
         val coroutineScope = rememberCoroutineScope()
         val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+        Scaffold(
+            modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        ) { innerPadding ->
+            EntryBodyCustomer(
+                addUIStateCustomer = addCustomerViewModel.addUIStateCustomer,
+                onCustomerValueChange = addCustomerViewModel::updateAddUIStateCustomer,
+                onSaveClickCustomer = {
+                    coroutineScope.launch {
+                        addCustomerViewModel.addCustomer()
+                        navigateBack()
+                    }
+                },
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxWidth()
+            )
+        }
     }
     @Composable
     fun EntryBodyCustomer(
