5bfda26be766bf9ba9d6f13d7503d6726b6c46c1
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-10 01:22:54 +0700

    membuat class AddCustomerViewModel dengan repository CustomerRepository dan mutableStateOf untuk state UI AddUIStateCustomer

--- app/src/main/java/com/example/projectakhir/ui/AddCustomer/AddCustomerVIewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/AddCustomer/AddCustomerVIewModel.kt
@@ -1,2 +1,13 @@
 package com.example.projectakhir.ui.AddCustomer
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.projectakhir.Data.CustomerRepository
+import com.example.projectakhir.ui.AddUIStateCustomer
+
+class AddCustomerViewModel(private val customerRepository: CustomerRepository) : ViewModel() {
+    var addUIStateCustomer by mutableStateOf(AddUIStateCustomer())
+        private set
+}
