83bedad7a63fbee89c9a0f97def2cefd7e573faa
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-11 00:24:39 +0700

    membuat class EditCustomerViewModel dan parameter nya

---
+++ app/src/main/java/com/example/projectakhir/ui/EditCustomer/EditCustomerViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.projectakhir.ui.EditCustomer
+
+import androidx.lifecycle.SavedStateHandle
+import com.example.projectakhir.Data.CustomerRepository
+
+class EditCustomerViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repository: CustomerRepository
+    ) {}
