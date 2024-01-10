fe31d5ec13a2287c441cc5f366a1e00f48d56d03
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-10 15:47:53 +0700

    menambahkan StateFlow uiStateCustomer untuk data Customer berdasarkan ID, dengan pemetaan ke DetailUIStateCustomer dan stateIn 6,000L

--- app/src/main/java/com/example/projectakhir/ui/DetailCustomer/DetailCustomerViewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/DetailCustomer/DetailCustomerViewModel.kt
@@ -2,7 +2,16 @@ package com.example.projectakhir.ui.DetailCustomer
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.projectakhir.Data.CustomerRepository
+import com.example.projectakhir.ui.DetailUIStateCustomer
+import com.example.projectakhir.ui.toDetailCustomer
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
 
 class DetailCustomerViewModel(
     savedStateHandle: SavedStateHandle,
@@ -12,4 +21,16 @@ class DetailCustomerViewModel(
         private const val TIMEOUT_MILLIS = 6_000L
     }
     val customerId: String = checkNotNull(savedStateHandle[DetailDestinationCustomer.customerId])
+
+    val uiStateCustomer: StateFlow<DetailUIStateCustomer> =
+        repository.getCustomerById(customerId)
+            .filterNotNull()
+            .map {
+                DetailUIStateCustomer(addEventCustomer = it.toDetailCustomer())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailUIStateCustomer()
+            )
+
 }
