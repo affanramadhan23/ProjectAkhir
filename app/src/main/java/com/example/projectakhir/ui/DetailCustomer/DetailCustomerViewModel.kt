d8585fbd4e510b102a8c657fbea53013d80c6250
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-10 15:41:53 +0700

    menambahkan class DetailCustomerViewModel dan parameter nya

--- app/src/main/java/com/example/projectakhir/ui/DetailCustomer/DetailCustomerViewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/DetailCustomer/DetailCustomerViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.projectakhir.ui.DetailCustomer
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.projectakhir.Data.CustomerRepository
+
+class DetailCustomerViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repository: CustomerRepository
+) : ViewModel() {}
