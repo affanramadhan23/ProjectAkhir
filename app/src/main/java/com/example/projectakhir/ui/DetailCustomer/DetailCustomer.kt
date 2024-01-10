be755cc18edc71581d3063be1335ca814013a6b9
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-10 15:51:29 +0700

    menambahkan AlertDialog untuk konfirmasi penghapusan, menggunakan TextButton 'Yes' dan 'No'

--- app/src/main/java/com/example/projectakhir/ui/DetailCustomer/DetailCustomer.kt
+++ app/src/main/java/com/example/projectakhir/ui/DetailCustomer/DetailCustomer.kt
@@ -1,5 +1,8 @@
 package com.example.projectakhir.ui.DetailCustomer
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.projectakhir.Navigation.DestinasiNavigasi
@@ -16,4 +19,21 @@ private fun DeleteConfirmationDialog(
     onDeleteConfirmCustomer: () -> Unit,
     onDeleteCancelCustomer: () -> Unit,
     modifier: Modifier = Modifier
-) {}
+) {
+    AlertDialog(
+        onDismissRequest = { /*TODO*/ },
+        title = { Text("Are you sure")},
+        text = { Text("Delete")},
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancelCustomer) {
+                Text(text = "No")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirmCustomer) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
