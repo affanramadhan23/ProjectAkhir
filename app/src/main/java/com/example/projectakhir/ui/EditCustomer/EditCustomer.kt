054ba97b135c67a9e93140b9ae993a0c928008f8
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-11 00:30:22 +0700

    membuat objek EditDestinationCustomer untuk rute 'item edit customer' dengan judul 'Edit Customer' dan argumen rute customerId

--- app/src/main/java/com/example/projectakhir/ui/EditCustomer/EditCustomer.kt
+++ app/src/main/java/com/example/projectakhir/ui/EditCustomer/EditCustomer.kt
@@ -1,2 +1,10 @@
 package com.example.projectakhir.ui.EditCustomer
 
+import com.example.projectakhir.Navigation.DestinasiNavigasi
+
+object EditDestinationCustomer : DestinasiNavigasi {
+    override val route = "item edit customer"
+    override val titleRes ="Edit Customer"
+    const val customerId = "customerId"
+    val routeWithArgs = "${EditDestinationCustomer.route}/{$customerId}"
+}
