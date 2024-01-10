703ffef67ac3058116b961786f6b1b8444cf5eca
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-11 00:28:07 +0700

    menambah variabel customerUIState

--- app/src/main/java/com/example/projectakhir/ui/EditCustomer/EditCustomerViewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/EditCustomer/EditCustomerViewModel.kt
@@ -1,9 +1,17 @@
 package com.example.projectakhir.ui.EditCustomer
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.projectakhir.Data.CustomerRepository
+import com.example.projectakhir.ui.AddUIStateCustomer
 
 class EditCustomerViewModel(
     savedStateHandle: SavedStateHandle,
     private val repository: CustomerRepository
-    ) {}
+) : ViewModel() {
+    var customerUIState by mutableStateOf(AddUIStateCustomer())
+        private set
+}
