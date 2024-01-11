12917d5edce20f1168b56cdc7da24a8704c3507b
affanramadhan23 <affannabilramadhan@gmail.com>
2024-01-11 12:20:22 +0700

    menambahkan initializer untuk EditCustomerViewModel, AddCustomerViewModel,DetailCustomerViewModel

--- app/src/main/java/com/example/projectakhir/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/PenyediaViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.projectakhir.ui
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.projectakhir.Pinjol.PinjolApplication
+import com.example.projectakhir.ui.AddCustomer.AddCustomerViewModel
+import com.example.projectakhir.ui.DetailCustomer.DetailCustomerViewModel
+import com.example.projectakhir.ui.EditCustomer.EditCustomerViewModel
 import com.example.projectakhir.ui.HomeCustomer.HalamanCustomerViewModel
 
 fun CreationExtras.aplikasiPinjol(): PinjolApplication =
@@ -15,6 +19,21 @@ object PenyediaViewModel {
 
         initializer {
             HalamanCustomerViewModel(aplikasiPinjol().container.customerRepository)
+        }
+        initializer {
+            AddCustomerViewModel(aplikasiPinjol().container.customerRepository)
+        }
+        initializer {
+            DetailCustomerViewModel(
+                createSavedStateHandle(),
+                aplikasiPinjol().container.customerRepository
+            )
+        }
+        initializer {
+            EditCustomerViewModel(
+                createSavedStateHandle(),
+                aplikasiPinjol().container.customerRepository
+            )
         }
     }
 }
